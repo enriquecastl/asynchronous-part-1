d88b97db522980b2b33950a8590e25972ab810f6
Enrique Alcantara <enriquealc27@gmail.com>
2015-05-01 20:41:57 -0400

    deleted project, starting again with a simpler concept

--- src/megusta/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var requester = require('./requester'),
-    processor = require('./processor')
-    ;
-
-module.exports = function megustaScraper() {
-    return requester().then(processor);
-};
