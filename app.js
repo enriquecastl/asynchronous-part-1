0c298f188a8805e0ea4da54052b6d626cd2d38cc
Enrique Alcantara <enriquealc27@gmail.com>
2015-05-01 20:59:56 -0400

    server that delives a request after x specified milliseconds

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+app.get(/\w+\.txt/, function(req, res) {
+	var waitUntil = parseInt(req.query.waitUntil, 10) || 10
+	
+	setTimeout(function() {
+		res.set('Content-Type', 'text/plain');
+		res.send('File content');
+	}, waitUntil);
+});
+
+var server = app.listen(3000, function(app) {
+	var host = server.address().address;
+  	var port = server.address().port;
+	
+	console.log('Listening at http://%s:%s', host, port);
+});
